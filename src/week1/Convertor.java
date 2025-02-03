e0ff490df90f6939722d5eab453cc5a30808876f
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-03 15:34:32 +0900

    feat: 10진수를 2진수 boolean 배열로 변환

--- src/week1/Convertor.java
+++ src/week1/Convertor.java
@@ -1,4 +1,26 @@
 package week1;
 
+import java.util.Arrays;
+
 public class Convertor {
+
+    public boolean[] dec2bin(int decInput) {
+        boolean[] bin = new boolean[8];
+        int count = 1;
+        int copyDec = decInput;
+        while (copyDec > 1) {
+            copyDec /= 2;
+            count++;
+        }
+        boolean[] answer = Arrays.copyOf(bin, count);
+        int pos = 0;
+        while (decInput > 0) {
+            if (decInput % 2 == 1) {
+                answer[pos] = true;
+            }
+            decInput /= 2;
+            pos++;
+        }
+        return answer;
+    }
 }
