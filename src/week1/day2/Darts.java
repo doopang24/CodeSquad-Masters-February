eef32e49fb2711947608b04bd197a9cb011fab32
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-05 00:03:36 +0900

    feat: 입력받은 게임이 몇 라운드인지 반환하는 기능 구현

--- src/week1/day2/Darts.java
+++ src/week1/day2/Darts.java
@@ -4,4 +4,7 @@ public class Darts {
 
     static final int[] SCORE_BOARD = new int[]{20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5, 20, 1};
     static final int SINGLE_ROUND = 7;
+    private int getRoundCount(int[] scoreLine) { // 게임 라운드 반환
+        return scoreLine.length / 7;
+    }
 }
