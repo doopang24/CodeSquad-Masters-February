b49c08484f56c8d255559db75be9b629d9b24bdc
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-05 00:00:54 +0900

    feat: 입력을 받는 클래스 생성

--- src/week1/day2/Main.java
+++ src/week1/day2/Main.java
@@ -1,4 +1,10 @@
 package week1.day2;
 
 public class Main {
+    private String getInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("점수를 쉼표와 띄어쓰기로 구분해서 입력하세요.");
+        return scanner.nextLine();
+    }
 }
