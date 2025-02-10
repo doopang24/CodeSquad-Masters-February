44837dd943a56910aecc70dcbbe1d363e7529327
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-10 22:41:23 +0900

    feat: Application 클래스 구현

--- src/week2/day1/Application.java
+++ src/week2/day1/Application.java
@@ -1,4 +1,38 @@
 package week2.day1;
 
+import java.util.Scanner;
+
 public class Application {
+
+    public int[] parseInput(String input) {
+        int[] attempt = new int[3];
+        String[] numbers = input.split(",");
+        for (int i = 0; i < numbers.length; i++) {
+            attempt[i] = Integer.parseInt(numbers[i].trim());
+        }
+        return attempt;
+    }
+
+    public String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        SudokuGenerator sudoku = new SudokuGenerator();
+        OutputHandler handler = new OutputHandler();
+
+        String input = application.getInput();
+        int[] attempt = application.parseInput(input);  // input 으로 사용자의 추축을 받음
+
+        int[][] gameBoard = sudoku.getGameBoard();
+        handler.printSudokuBoard(gameBoard);
+        System.out.println();
+        handler.printEasySudoku(gameBoard);
+        System.out.println();
+        handler.printHardSudoku(gameBoard);
+    }
 }
+
