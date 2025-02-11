377d3b81ed0ce5d4836a14de420d4c051fa7e43e
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-11 11:56:09 +0900

    refactor: Application 클래스 수정

--- src/week2/day1/Application.java
+++ src/week2/day1/Application.java
@@ -1,38 +1,21 @@
 package week2.day1;
 
-import java.util.Scanner;
-
 public class Application {
 
-    public int[] parseInput(String input) {
-        int[] attempt = new int[3];
-        String[] numbers = input.split(",");
-        for (int i = 0; i < numbers.length; i++) {
-            attempt[i] = Integer.parseInt(numbers[i].trim());
-        }
-        return attempt;
-    }
-
-    public String getInput() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        return input;
-    }
-
     public static void main(String[] args) {
-        Application application = new Application();
-        SudokuGenerator sudoku = new SudokuGenerator();
-        OutputHandler handler = new OutputHandler();
+        InputHandler inputHandler = new InputHandler();
+        SudokuGenerator generator = new SudokuGenerator();
+        OutputHandler outputHandler = new OutputHandler();
 
-        String input = application.getInput();
-        int[] attempt = application.parseInput(input);  // input 으로 사용자의 추축을 받음
-
-        int[][] gameBoard = sudoku.getGameBoard();
-        handler.printSudokuBoard(gameBoard);
-        System.out.println();
-        handler.printEasySudoku(gameBoard);
-        System.out.println();
-        handler.printHardSudoku(gameBoard);
+        outputHandler.printEasySudoku(generator.getGameBoard());
+        int[] attempt = inputHandler.startGame();
+        if (generator.canPlaceNumber(attempt)) {
+            /*
+            TODO
+            - 사용자가 입력(위치, 숫자)하면 현재 스도쿠 판에 들어갈 수 있는지 검증한 후, 해당 위치를 해당 숫자로 바꿔서 출력
+            - 사용자가 이미 채워진 위치에 있는 숫자를 바꾸고 싶으면 동일한 방식으로 검증하고 바꿔서 출력
+            - 그러다가 사용자가 정답(프로그램이 생성한 스도쿠 판)을 완성하면 완성했다는 안내 문구화 함께 프로그램 종료.
+             */
+        }
     }
 }
-
