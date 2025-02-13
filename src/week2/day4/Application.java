8dc1b8e5b86e210e11dd6dee67cc9a1b94e0dc96
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-13 18:29:06 +0900

    feat: Application 클래스 구현

--- src/week2/day4/Application.java
+++ src/week2/day4/Application.java
@@ -1,4 +1,22 @@
 package week2.day4;
 
+import java.util.Scanner;
+
 public class Application {
+
+    public int getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("확인할 단계를 입력하세요.");
+        return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        Printer printer = new Printer();
+        Transporter transporter = new Transporter();
+
+        int inputStep = application.getInput();
+        transporter.transportController(inputStep);
+
+    }
 }
