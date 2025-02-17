dea9ff123bd8c9579fa9f4492981af834c542ae1
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-17 18:38:42 +0900

    feat: LogList 클래스 구현

--- src/week3/day1/LogList.java
+++ src/week3/day1/LogList.java
@@ -1,4 +1,13 @@
 package week3.day1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LogList {
+
+    private static List<LogEntry> SystemLogList = new ArrayList<>();
+
+    public void addLogList(LogEntry logData) {
+        SystemLogList.add(logData);
+    }
 }
