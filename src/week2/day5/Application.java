c94f687e8aba9265172f07c4c849c7e9402af30b
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-17 12:37:34 +0900

    refactor: Application 클래스 수정

--- src/week2/day5/Application.java
+++ src/week2/day5/Application.java
@@ -1,12 +1,9 @@
 package week2.day5;
 
 public class Application {
-
-
     public static void main(String[] args) {
         InputHandler inputHandler = new InputHandler();
 
         inputHandler.getInput();
-
     }
 }
