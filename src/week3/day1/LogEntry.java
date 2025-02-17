2a41cb596842301bf74f565c1b1bdea3e8954d34
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-17 18:38:29 +0900

    feat: LogEntry 클래스 구현

--- src/week3/day1/LogEntry.java
+++ src/week3/day1/LogEntry.java
@@ -1,4 +1,17 @@
 package week3.day1;
 
+import java.time.LocalTime;
+
 public class LogEntry {
+    private String logLevel;
+    private LocalTime logTime;
+    private String logProcess;
+    private String logMessage;
+
+    public LogEntry(String logLevel, LocalTime logTime, String logProcess, String logMessage) {
+        this.logLevel = logLevel;
+        this.logTime = logTime;
+        this.logProcess = logProcess;
+        this.logMessage = logMessage;
+    }
 }
