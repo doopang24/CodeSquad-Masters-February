91ef59784a38ab3f952af44f35b07308eb9899f7
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-18 09:57:31 +0900

    feat: Application 클래스 수정

--- src/week3/day1/Application.java
+++ src/week3/day1/Application.java
@@ -5,10 +5,14 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         LogFileReader reader = new LogFileReader();
+        LogFilter filter = new LogFilter();
+
         try {
             reader.readLogFile();
         } catch (IOException e) {
             System.out.println("파일을 읽을 수 없습니다.");
         }
+
+        filter.startSearch();
     }
 }
