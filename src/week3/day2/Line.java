7fd8abc40f30badd36662ca3deb5323242cb96ca
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-19 10:53:32 +0900

    refactor: Line 클래스 수정

--- src/week3/day2/Line.java
+++ src/week3/day2/Line.java
@@ -1,27 +1,27 @@
 package week3.day2;
 
+import java.util.List;
+
 public class Line implements Figure {
 
-    private Coordinate point1, point2;
-    private String[][] grid = new String[51][51];
+    private List<Coordinate> coordinateList;
 
-    public Line(Coordinate point1, Coordinate point2) {
-        this.point1 = point1;
-        this.point2 = point2;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                grid[i][j] = " ";
-            }
-        }
+    public Line(List<Coordinate> coordinateList) {
+        this.coordinateList = coordinateList;
     }
 
     @Override
     public double calculateArea() {
+        Coordinate point1 = coordinateList.get(0);
+        Coordinate point2 = coordinateList.get(1);
         return Math.sqrt(Math.pow((point1.getX() - point2.getX()), 2) + Math.pow((point1.getY() - point2.getY()), 2));
     }
 
     @Override
     public void printResult() {
+        CoordinatePlane plane = new CoordinatePlane();
+        plane.displayResult(coordinateList);
+
         System.out.println("길이 : " + calculateArea());
     }
 }
