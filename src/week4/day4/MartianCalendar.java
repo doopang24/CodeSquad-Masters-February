1e3f770d4dbf2b3c4934afd58df211bab52a65ca
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-02-28 12:09:30 +0900

    refactor: MartianCalendar 클래스가 출력을 모두 담당하도록 수정

--- src/week4/day4/MartianCalendar.java
+++ src/week4/day4/MartianCalendar.java
@@ -1,16 +1,39 @@
 package week4.day4;
 
+import java.text.DecimalFormat;
+
 public class MartianCalendar {
 
-    // 연, 월을 주면 full 달력인지 아닌지 판단할 수 있게 만들기
-    public void fullCalendar(int year, int month) {
-        int lastDay = 28;
-        printCalendar(lastDay, year, month);
+    private final int MARS_NORMAL_MONTH_SIZE = 28;
+    private final int MARS_SIXFOLD_MONTH_SIZE = 27;
+
+    public void startConvert(int[] presentDay) {
+        DateUtils utils = new DateUtils();
+
+        long totalDays = utils.countDays(presentDay);   // 총 일수
+        int[] martianDate = utils.convertMartian(totalDays);    // 화성 날짜
+        printConversionResult(totalDays, martianDate);
+        decideCalendar(martianDate);
     }
 
-    public void sixfoldCalendar(int year, int month) {
-        int lastDay = 27;
-        printCalendar(lastDay, year, month);
+    private void printConversionResult(long totalDays, int[] martianDate) {
+        DecimalFormat pattern = new DecimalFormat("#,###");
+        String formatedDays = pattern.format(totalDays);
+        System.out.println("\n지구날은 " + formatedDays + " => " + martianDate[0] + " 화성년 " + martianDate[1] + "월 " + martianDate[2] + "일");
+        System.out.println();
+    }
+
+    private void decideCalendar(int[] martianDate) {
+        int year = martianDate[0], month = martianDate[1], day = martianDate[2];
+        if (month % 6 == 0) {
+            if (year % 2 == 0 && month % 24 == 0) {
+                printCalendar(MARS_NORMAL_MONTH_SIZE, year, month);
+            } else {
+                printCalendar(MARS_SIXFOLD_MONTH_SIZE, year, month);
+            }
+        } else {
+            printCalendar(MARS_NORMAL_MONTH_SIZE, year, month);
+        }
     }
 
     private void printCalendar(int lastDay, int year, int month) {
@@ -24,7 +47,7 @@ public class MartianCalendar {
                 if (i % 7 == 0) builder.append("\n");
             }
         }
-        System.out.println("     " + year + "년 " + month + "월" + "     ");
+        System.out.println("      " + year + "년 " + month + "월" + "     ");
         System.out.println(" Su Lu Ma Me Jo Ve Sa");
         System.out.println(builder.toString());
     }
